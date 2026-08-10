34576ba0c373d4bc64804a5ef64cc2bf88469142
anika-codes05 <tyagianika4252@gmail.com>
2026-08-10 22:19:58 +0530

    day2

--- patternPractice.java
+++ patternPractice.java
@@ -31,12 +31,45 @@ public class patternPractice{
             System.out.println();
         }
     }
+    //day-2 pattern 
+    public static void pattern4(int n){
+        System.out.println("pattern 4");
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }System.out.println();
+        }
+    }
+    //patern5
+    public static void pattern5(int n){
+    System.out.println("pattern 5: ");
+     for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i);
+            }System.out.println();
+        }
+    }
+    //pattern 6
+    public static void pattern6(int n){
+        System.out.println("pattern 6:");
+         for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(j);
+            }System.out.println();
+        }
+
+    }
+    
     public static void main(String args[]){
    Scanner sc=new Scanner(System.in);
+   System.out.println("enter the value of n: ");
          int n=sc.nextInt();
          pattern1(n);
          pattern2(n);
          pattern3(n);
+         pattern4(n);
+         pattern5(n);
+         pattern6(n);
 
     }
 }
